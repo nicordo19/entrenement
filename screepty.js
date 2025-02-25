e9d626e8a3c3f55b124fea18693e103645b643d6
nicordo19 <poiraudnico@gmail.com>
2025-02-25 15:15:54 +0100

    nouvelle version du jeux trouve le bon numero

--- screepty.js
+++ screepty.js
@@ -1,8 +1,7 @@
 // création de phrases aleatoirs
 // au click une phrase aléatoir doit apparaitre
 // tu a sujet verbe et complement
-let displayResult = document.querySelector(".result");
-let numbersDifferents = document.querySelector(".number");
+
 const button = document.querySelector("button");
 let eRandom = document.querySelector(".eRandom");
 
@@ -44,44 +43,66 @@ function addWordRandom() {
 }
 // choisire le bon nombre
 // tu a un prompte qui apparais tu a 7 chance pour trouver le bon numero
-numbersDifferents.addEventListener("change", (event)=>{
-  displayResult.textContent = 
-})
+let displayResult = document.querySelector(".result");
+let numbersDifferents = document.querySelector(".number");
 
-function numberRandom(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-let result = numberRandom(7);
-console.log(result);
+let misterNomber = Math.floor(Math.random() * 7) + 1;
+let chance = 7;
+numbersDifferents.addEventListener("change", (event) => {
+  let selectedNomber = Number(event.target.value);
+  let message = iisOkay(selectedNomber);
+  displayResult.textContent = message;
+});
 
 function iisOkay(n) {
-  return result === n;
-}
-// faire en sorte en cas de une mauvaise saisie
-function Gess() {
-  const number = prompt("entre un numero Entre 1 et 7 (tu a 7 chances))") * 1;
-  if (isNaN(number) || number < 0 || number > 7) {
-    console.log("Entre un nombre valide !");
-    return false;
+  if (chance === 0) {
+    return "tu n'a pas trouvé 😭 ,ne perd pas hespoir 💪 si tu veux retante ta chance  ";
   }
-  return number;
-}
-
-for (let i = 0; i < 7; i++) {
-  let numberUser = Gess();
-  if (numberUser === false) {
-    i--; // Ne pas compter un essai invalide
-    continue;
-  }
-
-  if (iisOkay(numberUser)) {
-    console.log("bravo tu a trouve");
-    break;
-  } else if (numberUser === result + 1 || numberUser === result - 1) {
-    console.log("tu été a un pret ");
+  chance--;
+  if (n === misterNomber) {
+    return "bravo tu a trouvé le bon numéro 🥳";
+  } else if (n === misterNomber - 1 || n === misterNomber + 1) {
+    return `tu été à coté de 1😳, il te rests ${chance}`;
   } else {
-    console.log("recommence mauvaise reponce");
+    return `retante ta chance 🙈 il te rests ${chance}`;
   }
+  //on reduit les essais a chaque tour
 }
 
-//projet 3 : Gestionnaire de produits
+// function numberRandom(max) {
+//   return Math.floor(Math.random() * (max + 1));
+// }
+// let result = numberRandom(7);
+// console.log(result);
+
+// function iisOkay(n) {
+//   return result === n;
+// }
+// // faire en sorte en cas de une mauvaise saisie
+// function Gess() {
+//   const number = prompt("entre un numero Entre 1 et 7 (tu a 7 chances))") * 1;
+//   if (isNaN(number) || number < 0 || number > 7) {
+//     console.log("Entre un nombre valide !");
+//     return false;
+//   }
+//   return number;
+// }
+
+// for (let i = 0; i < 7; i++) {
+//   let numberUser = Gess();
+//   if (numberUser === false) {
+//     i--; // Ne pas compter un essai invalide
+//     continue;
+//   }
+
+//   if (iisOkay(numberUser)) {
+//     console.log("bravo tu a trouve");
+//     break;
+//   } else if (numberUser === result + 1 || numberUser === result - 1) {
+//     console.log("tu été a un pret ");
+//   } else {
+//     console.log("recommence mauvaise reponce");
+//   }
+// }
+
+// //projet 3 : Gestionnaire de produits
